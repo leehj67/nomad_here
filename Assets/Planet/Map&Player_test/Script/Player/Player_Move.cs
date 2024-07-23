2f6827d959d599a1360ca960f7561a5a431651c7
Seydlitz-code <153933205+Seydlitz-code@users.noreply.github.com>
2024-07-23 12:24:09 +0900

    playermove test

--- Assets/Planet/Map&Player_test/Script/Player/Player_Move.cs
+++ Assets/Planet/Map&Player_test/Script/Player/Player_Move.cs
@@ -1,16 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Photon.Pun;
 
-public class Player_Move : MonoBehaviour
+public class Player_Move : MonoBehaviourPun, IPunObservable
 {
 	public float Speed;
-	public VariableJoystick joy;
 	Rigidbody2D rigid;
 	Animator anim;
 	float h;
 	float v;
 	bool isHorizonMove;
+
+	// 네트워크 동기화 변수
+	private Vector3 networkPosition;
+	private Quaternion networkRotation;
+	private Vector2 networkVelocity;
+	private float lag;
 
 	void Awake()
 	{
@@ -22,30 +28,38 @@ public class Player_Move : MonoBehaviour
 
 	void Update()
 	{
-		// 입력 값을 최신 상태로 업데이트
-		h = joy.Horizontal;
-		v = joy.Vertical;
+		if (photonView.IsMine)
+		{
+			// 키보드 입력 받기
+			h = Input.GetAxisRaw("Horizontal");
+			v = Input.GetAxisRaw("Vertical");
 
-		// 조이스틱 입력이 없을 때 키보드 입력 받기
-		if (Mathf.Approximately(h, 0f)) h = Input.GetAxisRaw("Horizontal");
-		if (Mathf.Approximately(v, 0f)) v = Input.GetAxisRaw("Vertical");
+			// 이동 방향 결정
+			isHorizonMove = Mathf.Abs(h) > Mathf.Abs(v);
 
-		// 이동 방향 결정
-		isHorizonMove = Mathf.Abs(h) > Mathf.Abs(v);
+			// 이동 상태 및 방향 애니메이션 설정
+			anim.SetBool("isMoving", !Mathf.Approximately(h, 0f) || !Mathf.Approximately(v, 0f));
+			anim.SetFloat("DirX", h);
+			anim.SetFloat("DirY", v);
 
-		// 이동 상태 및 방향 애니메이션 설정
-		anim.SetBool("isMoving", !Mathf.Approximately(h, 0f) || !Mathf.Approximately(v, 0f));
-		anim.SetFloat("DirX", h);
-		anim.SetFloat("DirY", v);
-
-		UpdateAnimation();
+			UpdateAnimation();
+		}
+		else
+		{
+			// 네트워크에서 수신한 위치 및 회전 적용
+			transform.position = Vector3.MoveTowards(transform.position, networkPosition, Time.deltaTime * Speed);
+			rigid.velocity = networkVelocity;
+		}
 	}
 
 	void FixedUpdate()
 	{
-		// 이동 수행
-		Vector2 moveVec = new Vector2(h, v).normalized;
-		rigid.velocity = moveVec * Speed;
+		if (photonView.IsMine)
+		{
+			// 이동 수행
+			Vector2 moveVec = new Vector2(h, v).normalized;
+			rigid.velocity = moveVec * Speed;
+		}
 	}
 
 	void UpdateAnimation()
@@ -69,7 +83,7 @@ public class Player_Move : MonoBehaviour
 
 	void OnTriggerEnter2D(Collider2D collision)
 	{
-		if (collision.CompareTag("Door_01"))
+		if (collision.CompareTag("Door_01") && photonView.IsMine)
 		{
 			GameObject door02 = GameObject.FindGameObjectWithTag("Door_02");
 			if (door02 != null)
@@ -78,6 +92,28 @@ public class Player_Move : MonoBehaviour
 				newPosition.x += 1; // X좌표를 1만큼 이동
 				transform.position = newPosition;
 			}
+		}
+	}
+
+	public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
+	{
+		if (stream.IsWriting)
+		{
+			// 데이터 송신
+			stream.SendNext(transform.position);
+			stream.SendNext(transform.rotation);
+			stream.SendNext(rigid.velocity);
+		}
+		else
+		{
+			// 데이터 수신
+			networkPosition = (Vector3)stream.ReceiveNext();
+			networkRotation = (Quaternion)stream.ReceiveNext();
+			networkVelocity = (Vector2)stream.ReceiveNext();
+
+			// 네트워크 지연 시간 계산
+			float lag = Mathf.Abs((float)(PhotonNetwork.Time - info.SentServerTime));
+			networkPosition += (Vector3)networkVelocity * lag;
 		}
 	}
 }
